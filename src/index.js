d4320c66b155a55d914b9b85f2b9dd281275bfef
mrluuucas <lucascleberlikedin@gmail.com>
2023-03-09 17:38:49 -0300

    javascript

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Function app
+functionApp()
+{
+  return
+  (
+    <>
+    </>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
